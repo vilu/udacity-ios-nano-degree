7934371171776da1a3cd075d29b80cdf2c147e6a
viktor.lund <viktor.lund@mobimeo.com>
2021-01-12 20:23:38 +0100

    Fix keyboard issue

--- OnTheMap/OnTheMap/ViewController/AddLocationViewController.swift
+++ OnTheMap/OnTheMap/ViewController/AddLocationViewController.swift
@@ -8,10 +8,23 @@ final class AddLocationViewController: UIViewController {
     @IBOutlet weak var findLocationButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    @IBOutlet weak var stackViewCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var stackViewBottomConstraint: NSLayoutConstraint!
+    
     private let geocoder: CLGeocoder = CLGeocoder()
     
     override func viewDidLoad() {
         findLocationButton.layer.cornerRadius = 5.0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     @IBAction func findLocationButtonOnTouchUpInside(_ sender: UIButton) {
@@ -20,7 +33,7 @@ final class AddLocationViewController: UIViewController {
             let link = linkTextField.text,
             !location.isEmpty,
             !link.isEmpty
-            else {
+        else {
             return
         }
         
@@ -28,7 +41,7 @@ final class AddLocationViewController: UIViewController {
         
         
         geocoder.geocodeAddressString(location) { (placemarks, error) in
-
+            
             self.activityIndicator.stopAnimating()
             
             if let error = error {
@@ -43,7 +56,7 @@ final class AddLocationViewController: UIViewController {
             guard
                 let placemarks = placemarks,
                 placemarks.count > 0
-                else {
+            else {
                 print("Failed to find coordinates for location \(location)")
                 let alert = UIAlertController.defaultAlert(
                     title: "Could not find coordinates for location",
@@ -72,7 +85,7 @@ final class AddLocationViewController: UIViewController {
                 self.present(alert, animated: true, completion: nil)
                 return
             }
-
+            
             confirmLocationViewController.location = location
             confirmLocationViewController.url = url
             confirmLocationViewController.coordinate = coordinate
@@ -83,14 +96,39 @@ final class AddLocationViewController: UIViewController {
             
             navigationController.pushViewController(confirmLocationViewController, animated: true)
         }
-        
-        
-        
-        
-
     }
     
     @IBAction func cancelButtonOnTap(_ sender: Any) {
         navigationController?.popToRootViewController(animated: true)
+    }
+    
+    private func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification:Notification) {
+        guard locationTextField.isFirstResponder || linkTextField.isFirstResponder else {
+            return
+        }
+        stackViewBottomConstraint.constant = getKeyboardHeight(notification)
+        NSLayoutConstraint.activate([stackViewBottomConstraint])
+        NSLayoutConstraint.deactivate([stackViewCenterYConstraint])
+    }
+    
+    @objc private func keyboardWillHide(_ notification:Notification) {
+        NSLayoutConstraint.deactivate([stackViewBottomConstraint])
+        NSLayoutConstraint.activate([stackViewCenterYConstraint])
+    }
+    
+    private func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
 }
